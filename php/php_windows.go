1ffcf21a47d2e25dc7236c54250bf40369a134fd
Yoby <logove@qq.com>
2021-05-02 18:16:24 +0800

    0

---
+++ php/php_windows.go
@@ -0,0 +1,45 @@
+// +build windows
+package php
+
+import (
+	"fmt"
+	"syscall"
+)
+func init(){
+	WinColor=WinColors
+}
+/**
+ * @Description: 支持windows设置颜色
+ * @param s 字符
+ * @param i1 颜色
+ */
+func WinColors(s string, i1 string){
+	c:=	map[string]int{
+		"black":0,
+		"blue":1,
+		"green":2,
+		"cyan":3,//青色
+		"red":4,
+		"purple":5,//紫色
+		"yellow":6,//
+		"white":15,
+		"gray":8,
+		"qing":3,
+		"zi":5,
+	}
+	var i int
+	if v, ok := c[i1]; ok {
+		i=v
+	}else{
+		i=4
+	}
+		var kernel32	*syscall.LazyDLL
+		kernel32=syscall.NewLazyDLL("kernel32.dll")
+		proc := kernel32.NewProc("SetConsoleTextAttribute")
+		handle, _, _ := proc.Call(uintptr(syscall.Stdout), uintptr(i))
+		fmt.Print(s)
+		handle, _, _ = proc.Call(uintptr(syscall.Stdout), uintptr(7))
+		CloseHandle := kernel32.NewProc("CloseHandle")
+		CloseHandle.Call(handle)
+}
+
